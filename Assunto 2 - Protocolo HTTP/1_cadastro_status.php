6b0fedcc8f6cfa797196da2e14adaf1512f126b2
MarianaGameiro113 <marianagameiro@outlook.com>
2026-09-14 13:44:46 -0300

    assunto 3
    assunto 3

--- Assunto 2 - Protocolo HTTP/1_cadastro_status.php
+++ Assunto 2 - Protocolo HTTP/1_cadastro_status.php
@@ -15,9 +15,28 @@
         <label for="idade">Idade:</label>
         <input type="number" name="idade" id="idade" required><br><br>
 
-        <input type="submit">Enviar</button>
+        <input type="submit" value="Enviar">
 
-        
     </form>
+
+<hr>
+<?php
+// $_SERVER["REQUEST_METHOD"] == "POST" verifica se o formulário foi enviado via POST
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = $_POST['nome'];
+    $idade = $_POST['idade'];
+
+    //tratamento de erro para idade
+    if($nome == '' || $idade == '') {
+        http_response_code(400); // Bad Request
+        echo "Erro: Nome e idade são obrigatórios.";
+    } elseif(!is_numeric($idade)) {
+        http_response_code(422); // Unprocessable Entity
+        echo "Erro: Idade deve ser um número.";
+    } else {
+        http_response_code(200); // OK
+        echo "Cadastro realizado com sucesso! Nome: $nome, Idade: $idade";
+    }
+}
 </body>
 </html>
